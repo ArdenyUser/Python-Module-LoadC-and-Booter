8df2bd292e3d73c356967e63d22bf3feb691c8a5
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-02-06 09:00:49 -0500

    added gui

--- Boot_Code/boothandle.py
+++ Boot_Code/boothandle.py
@@ -1,3 +1,17 @@
-import cksum
-import loadx
-import game_boot
+import PySimpleGUI as sg
+layout = [[sg.Text("Complete LoadX boot?")], [sg.Button("OK")]]
+# Create the window
+window = sg.Window("LoadX Boot", layout)
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "OK" or event == sg.WIN_CLOSED:
+        import cksum
+        import loadx
+        import game_boot
+        break
+
+window.close()
+
