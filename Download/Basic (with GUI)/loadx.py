bbd80f764889899ecd50c181726bcb58ecf8f795
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-02-06 15:03:39 -0500

    copyright

--- Download/Basic (with GUI)/loadx.py
+++ Download/Basic (with GUI)/loadx.py
@@ -9,3 +9,15 @@ def Output_log(log):
   file = open("output_log.txt", "a")
   file.write('\n' + log);
   file.close()
+def Reload_direct():
+  import game_boot
+def Reload_full():
+  import main_boot
+def LoadX_info():
+  print("LoadX Copyright 2021 by Avery Stafford, this is free software! GNU Free Software License")
+  print("!License Included!")
+  print("This is a module, or addon, if you are using the Full version, aka using the booter with this, it is all copyrighted: ")
+  print("LoadX Booter/Python-Module-LoadX-and-Booter Copyright 2021 by Avery Stafford, this is free software!")
+  print("!License Included!")
+  print("And, Tinker (or is it Tkinker) is not mine, nor PySimpleGui.")
+  
