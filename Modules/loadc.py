34745a509f8a5991bf397a51e8205f95f8991483
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-02-07 11:09:03 -0500

    copyright fix

--- Modules/loadc.py
+++ Modules/loadc.py
@@ -13,11 +13,11 @@ def Reload_direct():
   import game_boot
 def Reload_full():
   import boot_main
-def LoadX_info():
-  print("LoadX Copyright 2021 by Avery Stafford, this is free software! GNU Free Software License")
+def LoadC_info():
+  print("LoadC Copyright 2021 by Avery Stafford, this is free software! GNU Free Software License")
   print("!License Included!")
   print("This is a module, or addon, if you are using the Full version, aka using the booter with this, it is all copyrighted: ")
-  print("LoadX Booter/Python-Module-LoadX-and-Booter Copyright 2021 by Avery Stafford, this is free software!")
+  print("LoadC Booter/Python-Module-LoadC-and-Booter Copyright 2021 by Avery Stafford, this is free software!")
   print("!License Included!")
   print("And, Tinker (or is it Tkinker) is not mine, nor PySimpleGui.")
 def File_replace_data(file, data):  
