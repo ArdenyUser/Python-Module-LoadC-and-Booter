4a4245c43897f9106e0c4a4a94e2bf190295e5ea
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-02-07 11:14:19 -0500

    copyright fix

--- Download/Binary_Installer/install_binary.ps1
+++ Download/Binary_Installer/install_binary.ps1
@@ -1,8 +1,8 @@
-wget https://github.com/ArdenyUser/Python-Module-LoadX-and-Booter/raw/main/Download/Basic%20(with%20GUI)/compiled_scripts/boothandle.cpython-38.pyc
-wget https://github.com/ArdenyUser/Python-Module-LoadX-and-Booter/raw/main/Download/Basic%20(with%20GUI)/compiled_scripts/cksum.cpython-38.pyc
-wget https://github.com/ArdenyUser/Python-Module-LoadX-and-Booter/raw/main/Download/Basic%20(with%20GUI)/compiled_scripts/cksum_support.cpython-38.pyc
-wget https://raw.githubusercontent.com/ArdenyUser/Python-Module-LoadX-and-Booter/main/Download/Basic%20(with%20GUI)/compiled_scripts/game_boot.py
-wget https://raw.githubusercontent.com/ArdenyUser/Python-Module-LoadX-and-Booter/main/Download/Basic%20(with%20GUI)/compiled_scripts/helpful_info.txt
-wget https://raw.githubusercontent.com/ArdenyUser/Python-Module-LoadX-and-Booter/main/Download/Basic%20(with%20GUI)/compiled_scripts/license.txt
-wget https://github.com/ArdenyUser/Python-Module-LoadX-and-Booter/raw/main/Download/Basic%20(with%20GUI)/compiled_scripts/loadx.cpython-38.pyc
-wget https://raw.githubusercontent.com/ArdenyUser/Python-Module-LoadX-and-Booter/main/Download/Basic%20(with%20GUI)/compiled_scripts/output_log.txt
+wget https://github.com/ArdenyUser/Python-Module-LoadC-and-Booter/raw/main/Download/Basic%20(with%20GUI)/compiled_scripts/boothandle.cpython-38.pyc
+wget https://github.com/ArdenyUser/Python-Module-LoadC-and-Booter/raw/main/Download/Basic%20(with%20GUI)/compiled_scripts/cksum.cpython-38.pyc
+wget https://github.com/ArdenyUser/Python-Module-LoadC-and-Booter/raw/main/Download/Basic%20(with%20GUI)/compiled_scripts/cksum_support.cpython-38.pyc
+wget https://raw.githubusercontent.com/ArdenyUser/Python-Module-LoadC-and-Booter/main/Download/Basic%20(with%20GUI)/compiled_scripts/game_boot.py
+wget https://raw.githubusercontent.com/ArdenyUser/Python-Module-LoadC-and-Booter/main/Download/Basic%20(with%20GUI)/compiled_scripts/helpful_info.txt
+wget https://raw.githubusercontent.com/ArdenyUser/Python-Module-LoadC-and-Booter/main/Download/Basic%20(with%20GUI)/compiled_scripts/license.txt
+wget https://github.com/ArdenyUser/Python-Module-LoadC-and-Booter/raw/main/Download/Basic%20(with%20GUI)/compiled_scripts/loadc.cpython-38.pyc
+wget https://raw.githubusercontent.com/ArdenyUser/Python-Module-LoadC-and-Booter/main/Download/Basic%20(with%20GUI)/compiled_scripts/output_log.txt
